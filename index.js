6b9b1fbb5010e19dfdeed23b0ef352c5d3513d92
ishanjarwal <ishan.professional01@gmail.com>
2024-01-08 10:46:34 +0530

    cache control set

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
-app.use(express.static(path.resolve(__dirname, "dist")))
+app.use(express.static(path.resolve(__dirname, "dist"), {
+    setHeaders: function (res, path, stat) {
+        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    }
+}))
 
 app.listen(8080, (err) => {
     if (err) {
